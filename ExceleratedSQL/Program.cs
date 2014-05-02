74647f26d021fb9aae237b0bd889b697f2c0f83b
AaronOziel <aaron.oziel@gmail.com>
2014-05-01 23:57:11 -0700

    Mostly finished
    Polished and mostly perfect. Working as intended just need some more
    error checking and testing.

--- ExceleratedSQL/Program.cs
+++ ExceleratedSQL/Program.cs
@@ -1,5 +1,5 @@
 ﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
-|*  Author: Aaron Oziel (May 2014)
+|*  Author: Aaron Oziel (April/May 2014)
 |*  Project: (Excel)eratedSQL
 |*  Description:
 |*      "Data needs to be moved from a spreadsheet to a database automatically. 
@@ -13,28 +13,19 @@
 |*  Tools:
 |*  [C#]: Considering this project is dealing with two different Microsoft
 |*  products (Excel and SQL Server) I thought it best to use Microsoft's 
-|*  favorite langauge. 
+|*  favorite langauge. Other than that I used the obvious SQL and Excel
+|*  libraries (System.Data.SqlClient & Microsoft.Office.Interop.Excel).
+|* 
 |*  [Links]:
 |*  http://www.codeproject.com/Tips/696864/Working-with-Excel-Using-Csharp
 |*  http://www.codeproject.com/Articles/4416/Beginners-guide-to-accessing-SQL-Server-through-C
 |*  
-|* 
 |*  Assumptions & Conditions: 
 |*      - The excel file will be exactly 6 columns in width
 |*      - Each cell will have approptiate data in it (ie: no strings in number cells)
 |*      - ID column must be unique (it is the PK for the table)
 |*      - Only the [Date Created] and [Rank] columns can be null
 |*      - Empty rows will cause errors. Never enter data in a cell you don't intend to use. 
-|* 
-|*  
-|*  Sample insert command:
-|*        cmd.Parameters["@Deck"].Value = "G/W Aggro";
-|*        cmd.Parameters["@Date"].Value = "10/12/2012";
-|*        cmd.Parameters["@Rank"].Value = 2;
-|*        cmd.Parameters["@Player"].Value = "Aaron Oziel";
-|*        cmd.Parameters["@ID"].Value = 955288156;
-|*        cmd.Parameters["@Pro"].Value = 0;
-|*        cmd.ExecuteNonQuery();
 |*
 \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
@@ -45,6 +36,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Diagnostics;
 
 namespace ExceleratedSQL
 {
@@ -57,21 +50,25 @@ namespace ExceleratedSQL
             // Setup absolute file path
             string path = null;
             if (args.Length == 0)
-                path = @"C:\Users\Aaron\Documents\Visual Studio 2013\Projects\ExceleratedSQL\ExceleratedSQL\ExceleratedSQL.xlsx";
+                path = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\ExceleratedSQL.xlsx";
             else
                 path = args[0];
 
             // Create Excel variables 
             Excel.Application excel = null;
             Excel.Workbook wkb = null;
-            int lastcol, lastrow;
+            Excel.Worksheet sheet = null;
+            int lastcol, lastrow = 0;
 
             try // Open file and read out important information  
             {
+                // Check for existance of file
+                if (!File.Exists(path))
+                    throw new FileNotFoundException();
                 excel = new Excel.Application();
                 excel.Visible = false;
                 wkb = excel.Workbooks.Open(path);
-                Excel.Worksheet sheet = (Excel.Worksheet)wkb.Sheets[1];
+                sheet = (Excel.Worksheet)wkb.Sheets[1];
                 // Get dimensions of relavent data
                 lastcol = sheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Column;
                 lastrow = sheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
@@ -79,16 +76,22 @@ namespace ExceleratedSQL
                 if (lastcol < MAX_COL)
                 {   // Not enough? End program. 
                     Console.WriteLine("[Error  #100] Excel document did not have enough columns.");
+                    Console.ReadLine();
                     return;
                 }
                 else if (lastcol > MAX_COL)
                 {   // Too many? Show a warning.
                     Console.WriteLine("[Warning#100] Excel document had more columns than necessary, columns" + MAX_COL + 1 + "+ will be ignored.");
                 }
+
+                Console.WriteLine("Opened Excel File Successfully");
             }
             catch (Exception e)
             {
                 Console.WriteLine("[Error  #101] " + e.ToString());
+                if (wkb != null) wkb.Close(); // Close open sheet
+                if (excel != null) excel.Quit(); // Close open excel application
+                Console.ReadLine();
                 return;
             }
 
@@ -101,11 +104,15 @@ namespace ExceleratedSQL
             {   // This part of the program is in need of help, too implimentation specific
                 myConnection = new SqlConnection(Properties.Settings.Default.ConnetionString);
                 myConnection.Open();
-
+                Console.WriteLine("Connected to SQL Database Successfully");
             }
             catch (Exception e)
-            {   // If connection fail end program. 
+            {   // If connection fails end program. 
                 Console.WriteLine("[Error  #200] " + e.ToString());
+                if (wkb != null) wkb.Close(); // Close open sheet
+                if (excel != null) excel.Quit(); // Close open excel application
+                if (myConnection != null) myConnection.Close(); // Close open SQL connection
+                Console.ReadLine();
                 return;
             }
 
@@ -115,11 +122,12 @@ namespace ExceleratedSQL
             cmd.Parameters.Add("@Date", System.Data.SqlDbType.DateTime);
             cmd.Parameters.Add("@Rank", System.Data.SqlDbType.SmallInt);
             cmd.Parameters.Add("@Player", System.Data.SqlDbType.VarChar, 50);
-            cmd.Parameters.Add("@ID",   System.Data.SqlDbType.Int);
-            cmd.Parameters.Add("@Pro",  System.Data.SqlDbType.Bit);
+            cmd.Parameters.Add("@ID", System.Data.SqlDbType.Int);
+            cmd.Parameters.Add("@Pro", System.Data.SqlDbType.Bit);
 
             try // Read every row in the spreadsheet and populate the database
             {
+                int inserts = 0;
                 for (int i = 1; i <= lastrow; i++)
                 {
                     if (Excel.Range.Equals(excel.get_Range("A" + i, Type.Missing), "")) // TODO: Is a blank cell "" or null?
@@ -127,37 +135,47 @@ namespace ExceleratedSQL
                         Console.WriteLine("[Warning#201] Row " + i + " seems to be blank. It was skipped.");
                     }
                     else
-                    {
-                        cmd.Parameters["@Deck"].Value   = excel.get_Range("A" + i, Type.Missing).Text;
+                    {   
+                        cmd.Parameters["@Deck"].Value = excel.get_Range("A" + i, Type.Missing).Text;
                         cmd.Parameters["@Date"].Value = Convert.ToDateTime(excel.get_Range("B" + i, Type.Missing).Text);
                         cmd.Parameters["@Rank"].Value = excel.get_Range("C" + i, Type.Missing).Text;
                         cmd.Parameters["@Player"].Value = excel.get_Range("D" + i, Type.Missing).Text;
                         cmd.Parameters["@ID"].Value = excel.get_Range("E" + i, Type.Missing).Text;
                         cmd.Parameters["@Pro"].Value = excel.get_Range("F" + i, Type.Missing).Text;
                         cmd.ExecuteNonQuery();
+                        inserts++;
                     }
                 }
+                Console.WriteLine("Inserted [" + inserts + "] entries into the database");
             }
-            catch (Exception e)
+            catch (System.Data.SqlClient.SqlException sql)
             {
-                Console.WriteLine("[Error  #201] " + e.ToString());
-            }
-            
-            try // Close everything, if this fails a lot of rogue processes will be running. 
-            {
-                TestSQL(myConnection);
-                myConnection.Close();
-                wkb.Close();
-                excel.Quit();
+                Console.WriteLine("[Error  #201] " + sql.ToString());
             }
             catch (Exception e)
             {
                 Console.WriteLine("[Error  #202] " + e.ToString());
             }
 
+            try // Close everything, if this fails a lot of rogue processes will be running. 
+            {
+                //TestSQL(myConnection);
+                if (wkb != null) wkb.Close(); // Close open sheet
+                if (excel != null) excel.Quit(); // Close open excel application
+                if (myConnection != null) myConnection.Close(); // Close open SQL connection
+            }
+            catch (Exception e) // This should never be used. 
+            {
+                Console.WriteLine("[Error  #202] " + e.ToString());
+            }
+            Console.WriteLine("--------------------");
+            Console.WriteLine("| Program Complete |");
+            Console.WriteLine("--------------------");
             Console.ReadLine();
         }
 
+        // Simple test function that will print the [Deck Name] of evrey entry in the database. 
+        // Used to test and see if inserts were truely successful. 
         public static void TestSQL(SqlConnection myConnection)
         {
             try
@@ -174,15 +192,6 @@ namespace ExceleratedSQL
             {
                 Console.WriteLine(e.ToString());
             }
-        }
-
-        // http://social.msdn.microsoft.com/Forums/vstudio/en-US/78e75d1a-0795-4bdb-8a62-ae6faa909986/convert-number-to-alphabet
-        // This method is taken from this URL by User "Manivannan.D.Sekaran" who wrote this on Monday, June 04, 2007 10:09 AM
-        // Renamed: "Column To Letter"
-        public static String colToLetter(int number, bool isCaps)
-        {
-            Char c = (Char)((isCaps ? 65 : 97) + number);
-            return c.ToString();
         }
     }
 }
