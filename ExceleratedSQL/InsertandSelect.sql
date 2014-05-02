3944aeeec67f82ee8eacecf3cad05e7554d7d0b0
AaronOziel <aaron.oziel@gmail.com>
2014-05-01 19:12:39 -0700

    Readding all files.

---
+++ ExceleratedSQL/InsertandSelect.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO Decks ([Deck Name], [Date Created], Rank, [Player Name], ID, Pro) VALUES('Boros Control', '10/12/2012', 12, 'Nihilus Aurix', 955282856, 1);
+SELECT * FROM Decks;
---
+++ ExceleratedSQL/SQLQuery1.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO Decks ([Deck Name], [Date Created], Rank, [Player Name], ID, Pro) VALUES('badrdw', 10/12/2012, 2, 'aaronoziel', 955281856, 0);
